6c2e477b24906a1879e9902e8b7a8f6ed080e751
Victor Santiago <victorsantiagoaraujo8@gmail.com>
2026-07-29 21:42:45 -0600

    Api de autenticacion

---
+++ AuthenticationAPI/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using AuthenticationAPI.interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/users")]
+[Authorize]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _service;
+    public UsersController(IUserService service)
+    {
+        _service = service;
+    }
+    [HttpGet]
+    [Authorize(Roles = "Administrador")]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok(await _service.GetAllAsync());
+    }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(string id)
+    {
+        var user = await _service.GetByIdAsync(id);
+        if (user == null)
+            return NotFound();
+        return Ok(user);
+    }
+    [HttpDelete("{id}")]
+    [Authorize(Roles = "Administrador")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        return Ok(await _service.DeleteAsync(id));
+    }
+}
---
+++ AuthenticationAPI/interfaces/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using AuthenticationAPI.DTO;
+
+namespace AuthenticationAPI.interfaces
+{
+    public interface IAuthService
+    {
+        Task<AuthResponseDto> RegisterAsync (RegisterRequestDto dto);
+        Task<AuthResponseDto> LoginAsync(LoginRequestDto dto);
+        Task LogoutAsync(string userId);
+    }
+}
